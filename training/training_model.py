8363ecb55d1a4d71dd07e2e82daee9ad4b86976d
Ali Siddiqi <alisiddiqui275@gmail.com>
2024-10-16 14:24:05 +0500

    dataset

--- training/training_model.py
+++ training/training_model.py
@@ -1,1 +1,22 @@
+
 import tensorflow as tf
+from tensorflow.python import keras
+from tensorflow.python.keras import models,layers
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import HTML
+
+
+
+
+BATCH_SIZE = 32
+IMAGE_SIZE = (256,256)
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "PlantVillage",
+    batch_size=BATCH_SIZE,
+    image_size=IMAGE_SIZE,
+    shuffle=True,
+    seed=24
+)
+
