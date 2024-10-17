48876eb5c2022e537c54f5bd3032015ad64ddf13
Ali Siddiqi <alisiddiqui275@gmail.com>
2024-10-17 22:42:21 +0500

    hmm

--- training/training_model.py
+++ training/training_model.py
@@ -38,37 +38,3 @@ EPOCHS = 50
 # 80 (Training)
 # 10 (Validation)
 # 10 (Testing)
-
-
-def get_partitioned_datasets(dataset,train_split=0.8, val_split= 0.1, test_split=0.1, shuffle= True, shuffle_size=10000):
-    if shuffle:
-        dataset = dataset.shuffle(shuffle_size, seed=10)
-
-    dataset_len= len(dataset)
-
-    train_len = int(train_split*dataset_len)
-    train_dataset= dataset.take(train_len)    
-    dataset = dataset.skip(train_len)
-
-
-    val_len = int(val_split*dataset_len)
-    val_dataset= dataset.take(val_len)    
-    dataset = dataset.skip(val_len)  
-
-    test_len = dataset_len - train_len - val_len
-    test_dataset = dataset.take(test_len)
-    
-    
-    return train_dataset, val_dataset, test_dataset
-
-
-train_dataset, val_dataset, test_dataset = get_partitioned_datasets(dataset)
-
-
-#print(len(train_dataset))
-
-train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size =tf.data.AUTOTUNE)
-val_dataset = val_dataset.cache().shuffle(1000).prefetch(buffer_size =tf.data.AUTOTUNE)
-test_dataset = test_dataset.cache().shuffle(1000).prefetch(buffer_size =tf.data.AUTOTUNE)
-
-
